e3892e5793012c0c89907bae24a0a94f60d5c0d4
Lutton <82480124+Lutton-alliance@users.noreply.github.com>
2021-04-14 22:38:07 -0400

    Updated true/false for t-notify dependency

--- config.lua
+++ config.lua
@@ -1,20 +1,11 @@
-Config = {
-    -----------------------------------------------------------------
-    armournumber = 6, -- Check vMenu for number of vest selected
-    -----------------------------------------------------------------
-    armourtexture = 1, -- Check vMenu for texture number of vest selected
-    -----------------------------------------------------------------
-    armourtimer = 7, -- How long in seconds to get armour and weapons.
-    -----------------------------------------------------------------
-    armourlevel = 50, -- 1-100 number for player armour given for vest chosen
-    -----------------------------------------------------------------
-
-    -----------------------------------------------------------------
-
-    -----------------------------------------------------------------
-
-    -----------------------------------------------------------------
-
-    -----------------------------------------------------------------
-    
- }
+Config = {}
+-------------------------------------------------
+Config.tnotify = true -- Set to True if you want to use t-notify for notifications
+-------------------------------------------------
+Config.armournumber = 6 -- Check vMenu for number of vest selected
+-------------------------------------------------
+Config.armourtexture = 1 -- Check vMenu for texture number of vest selected
+-------------------------------------------------
+Config.armourtimer = 6 -- How long in seconds to get armour and weapons.
+-------------------------------------------------
+Config.armourlevel = 50 -- 1-100 number for player armour given for vest chosen
