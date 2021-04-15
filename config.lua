f15c349b8c998906fa462ce95891b383db1fea79
Lutton <82480124+Lutton-alliance@users.noreply.github.com>
2021-04-15 14:27:58 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -9,3 +9,5 @@ Config.armourtexture = 1 -- Check vMenu for texture number of vest selected
 Config.armourtimer = 6 -- How long in seconds to get armour and weapons.
 -------------------------------------------------
 Config.armourlevel = 50 -- 1-100 number for player armour given for vest chosen
+-------------------------------------------------
+Config.imagenotify = 'https://cdn.discordapp.com/attachments/712448149943353356/832268992609124412/Untitled.png' -- Link to image that will show up on T-notify notifications
